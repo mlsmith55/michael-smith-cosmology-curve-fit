192bcdb410b5d8bcf1509d971808031ced844cbc
Martin Stålberg <mast4461@gmail.com>
2020-12-28 15:47:40 +0100

    fix: use relative path for data import

--- WeightedRegression.py
+++ WeightedRegression.py
@@ -14,7 +14,7 @@ import scipy.optimize as optimize
 import numpy as np
 #from np import sqrt, sinh, arctanh, fabs 
 
-DF=pd.read_csv(r'C:\Users\Mike\Desktop\SNdata\Riess1998_DL_Data.csv')
+DF=pd.read_csv('Riess1998_DL_Data.csv')
 #Now check to make certain the proper file is being read
 DF.info()
 
