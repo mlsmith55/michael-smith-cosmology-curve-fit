c4ab304008fde35d112e5f2804cebf59731aba9f
Martin Stålberg <mast4461@gmail.com>
2020-12-30 14:31:39 +0100

    refactor: prepend some private variables and functions with underscore to indicate that they are private

--- models/logDE_mag.py
+++ models/logDE_mag.py
@@ -7,21 +7,21 @@ The function below is the exact function decribing the FLRW model with two param
 ###########
 This second portion is the function to be integrated which is mag version of equation E5 of DOI: 10.5772/intechopen.91266. The next definition is the function needed for integration
 """
-def portion2(Z, Matter):
+def _portion2(Z, Matter):
     return 1/(math.sqrt((1+Z)**2)*(1+Matter*Z) - 2*(2+Z)*math.fabs(1-Matter))
 
 """
 This definition is the integration process, note the integration is from 0 to Z and not 1 to Z as per the equation E5.
 """
-def intersum(Z, Matter):
-    return quad(portion2, 0, Z, args=(Matter))[0]
-vectorizedIntersum = np.vectorize(intersum, excluded=["Matter"])
+def _intersum(Z, Matter):
+    return quad(_portion2, 0, Z, args=(Matter))[0]
+_vectorizedIntersum = np.vectorize(_intersum, excluded=["Matter"])
 
 """
 This is a simple calculation of the pre-integral portion of E5
 """
-def portion1(Z, Hubble):
+def _portion1(Z, Hubble):
     return 299792*(1+Z)/Hubble
 
 def model(Z, Hubble, Matter):
-    return (5*np.log10(portion1(Z, Hubble))*(vectorizedIntersum(Z, Matter))+25)
+    return (5*np.log10(_portion1(Z, Hubble))*(_vectorizedIntersum(Z, Matter))+25)
