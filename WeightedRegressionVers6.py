30890bb8c4241212e65f5761dc19a978df662936
Martin Stålberg <mast4461@gmail.com>
2020-12-30 10:42:06 +0100

    wip: attempt to fix WeightedRegressionVers6 with new math from Michael
    It's still not working
    
    (Also remove superfluous comment)

--- WeightedRegressionVers6.py
+++ WeightedRegressionVers6.py
@@ -35,30 +35,26 @@ The function below is the exact function decribing the FLRW model with two param
 This second portion is the function to be integrated which is mag version of equation E5 of DOI: 10.5772/intechopen.91266. The next definition is the function needed for integration
 """
 def portion2(x, Matter):
-    return 1/(x*math.sqrt((Matter/x) + math.fabs(1-Matter)*x**2))
+    return 1/(math.sqrt((1+x)**2)*(1+Matter*x) - 2*(2+x)*math.fabs(1-Matter))
 
 """
-This definition is the integration process, note the integration is from x to 1 and not 1 to x as per the equation E5.
+This definition is the integration process, note the integration is from 0 to x and not 1 to x as per the equation E5.
 """
 def intersum(x, Matter):
-    return quad(portion2, x, 1, args=(Matter))[0]
+    return quad(portion2, 0, x, args=(Matter))[0]
 vectorizedIntersum = np.vectorize(intersum, excluded=["Matter"])
 
 """
 This is a simple calculation of the pre-integral portion of E5
 """
 def portion1(x, Hubble):
-    return (299792/(x*Hubble))
+    return 299792*(1+x)/Hubble
 
 """
 putting the complete function together as the func
 """
 def func(x, Hubble, Matter):
     return (5*np.log10(portion1(x, Hubble))*(vectorizedIntersum(x, Matter))+25)
-
-"""
-I suppose that func can be used with curve_fit ?
-"""
 
 popt, pcov = optimize.curve_fit(func, x, y, sigma=w, p0=[70, 0.5], bounds=([60,0.01],[80,0.99]))
 fitLabel = 'fit: Hubble=%5.3f, Matter=%5.3f' % (popt[0], popt[1])
