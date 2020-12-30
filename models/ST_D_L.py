d5c7fa7d8e12c1e4cf27480926ba64faf21328b9
Martin Stålberg <mast4461@gmail.com>
2020-12-30 14:50:01 +0100

    refactor: merge the model ST_D_L with the new version sent by Mike

--- models/ST_D_L.py
+++ models/ST_D_L.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 14:22:19 2020
+
+@author: Mike
+"""
+
 import numpy as np
 
+"""
+This routine analyses D_L_Data for the CURVED UNIVERSE WITHOUT DARK ENERGY.
+"""
 def model(D_L, Hubble, Matter):
-    x = D_L
-    return (299792/(Hubble*x*np.sqrt(np.fabs(1-Matter)))*np.sinh(2*(np.arctanh(np.sqrt(np.fabs(1-Matter)))-np.arctanh(np.sqrt(np.fabs(1-Matter))/np.sqrt((Matter/x)+np.fabs(1-Matter))))))
+    return (299793/(Hubble*D_L*np.sqrt(np.fabs(1-Matter)))*np.sinh(2*(np.arctanh(np.sqrt(np.fabs(1-Matter)))-np.arctanh(np.sqrt(np.fabs(1-Matter))/np.sqrt((Matter/D_L)+np.fabs(1-Matter))))))
