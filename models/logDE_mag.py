584cf081946d9d3000e6de02e8d7cfab61106ec4
Martin Stålberg <mast4461@gmail.com>
2020-12-30 14:57:12 +0100

    style: remove some spurious whitespace

--- models/logDE_mag.py
+++ models/logDE_mag.py
@@ -22,7 +22,7 @@ def model(Z, Hubble, Matter):
 
 """
 This second portion is the function to be integrated which is mag version of equation E5 of DOI: 10.5772/intechopen.91266. The next definition is the function needed for integration
-"""  
+"""
 def _portion2(Z, Matter):
     return 1/(math.sqrt((1+Z)**2)*(1+Matter*Z) - 2*(2+Z)*math.fabs(1-Matter))
 
