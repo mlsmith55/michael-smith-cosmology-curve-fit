36be426180e0272341b0341809555e5b268af46a
Martin Stålberg <mast4461@gmail.com>
2020-12-30 15:07:18 +0100

    refactor: reformat model InterST_D_L

--- models/InterST_D_L.py
+++ models/InterST_D_L.py
@@ -5,45 +5,29 @@ Created on Mon Dec 28 17:28:35 2020
 @author: Mike
 """
 
-#import numpy as np
-import pandas as pd
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-import scipy
-#import scipy.integrate as integrate
 from scipy.integrate import quad
-"""
-Importing SNe Ia data from a csv file, below. The info command is run to double check that the correct file has been called.
-"""
-DF=pd.read_csv(r'C:/Users/Mike/Desktop/MartinS/SNdata/Riess1998_DL_Data.csv')
-#Now check to make certain the proper file is being read
-DF.info()
 from settings import speed_of_light
 
-x = DF['ExpFact']
-y = DF['D_L']
-w = DF['Err_D_L']
 """
 The function below is the exact function decribing the FLRW model with two parameters, Hubble constant and matter density. Note this is a 2-parameter FIT OF CURVED SPACETIME WITHOUT DARK ENERGY
-###########
+"""
+def model(x, Hubble, Matter):
+    return (_portion1(x, Hubble, Matter))*math.sinh(math.sqrt(math.fabs(1-Matter))*(_vectorizedIntersum(x, Matter)))
+
+"""
 This second portion is the function to be integrated
-"""  
-def portion2(x, Matter):
+"""
+def _portion2(x, Matter):
     return 1/(x*math.sqrt((Matter/x) + math.fabs(1-Matter)))
-                     
-def intersum(x):
-    return quad(lambda x: portion2, x, 1)
+
+def _intersum(x, Matter):
+    return quad(_portion2, x, 1, args=(Matter))
+_vectorizedIntersum = np.vectorize(_intersum, excluded=["Matter"])
+
 """
 This first portion is a simple calculation
 """
-def portion1(x, Hubble, Matter):
-    return speed_of_light/(x*Hubble *math.sqrt(math.fabs(1-Matter))
-"""
-putting the complete function together as the cmpltresult
-"""
-def cmpltresult(x, Hubble, Matter):
-    return ((portion1)*math.sinh(math.sqrt(math.fabs(1-Matter))*(intersum)))          
-"""
-TypeError: unsupported operand type(s) for *: 'function' and 'function'
-"""
+def _portion1(x, Hubble, Matter):
+    return speed_of_light/(x*Hubble *math.sqrt(math.fabs(1-Matter)))
