59c623f75f98b5dd05da917faf996ffe46ba3cd8
Martin Stålberg <mast4461@gmail.com>
2020-12-30 14:43:31 +0100

    refactor: merge logDE_mag with the new version sent from Mike

--- models/logDE_mag.py
+++ models/logDE_mag.py
@@ -1,12 +1,28 @@
-import math
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 17:28:35 2020
+
+@author: Mike
+"""
+
+import pandas as pd
 import numpy as np
+import math
 from scipy.integrate import quad
 
 """
 The function below is the exact function decribing the FLRW model with two paramters, Hubble constant and matter density. Note this is a 2-parameter FIT OF FLAT SPACETIME WITH DARK ENERGY
-###########
-This second portion is the function to be integrated which is mag version of equation E5 of DOI: 10.5772/intechopen.91266. The next definition is the function needed for integration
+
+Z is the redshift astronomers wrongly use as the recession velocity.
+m_B is the "distance magnitude" from signals using a blue filter
+Error_m_B is the standard deviation about m_B
 """
+def model(Z, Hubble, Matter):
+    return 5*np.log10(_portion1(Z, Hubble))*(_vectorizedIntersum(Z, Matter))+25
+
+"""
+This second portion is the function to be integrated which is mag version of equation E5 of DOI: 10.5772/intechopen.91266. The next definition is the function needed for integration
+"""  
 def _portion2(Z, Matter):
     return 1/(math.sqrt((1+Z)**2)*(1+Matter*Z) - 2*(2+Z)*math.fabs(1-Matter))
 
@@ -22,6 +38,3 @@ This is a simple calculation of the pre-integral portion of E5
 """
 def _portion1(Z, Hubble):
     return 299792*(1+Z)/Hubble
-
-def model(Z, Hubble, Matter):
-    return (5*np.log10(_portion1(Z, Hubble))*(_vectorizedIntersum(Z, Matter))+25)
