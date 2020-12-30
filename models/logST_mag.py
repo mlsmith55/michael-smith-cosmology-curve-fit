72edb572f2da0100bbd3b3d1cfe822515c2b3fe5
Martin Stålberg <mast4461@gmail.com>
2020-12-30 14:47:18 +0100

    refactor: merge logST_mag with the new version sent from Mike

--- models/logST_mag.py
+++ models/logST_mag.py
@@ -6,16 +6,16 @@ Created on Mon Dec 28 17:28:35 2020
 """
 
 import numpy as np
-"""
-Importing SNe Ia data from a csv file, below. The info command is run to double check that the correct file has been called.
-"""
-# DF=pd.read_csv(r'C:/Users/Mike/Desktop/MartinS/SNdata/Riess1998_mag_Data.csv')
 
 """
 The function below is the exact function decribing the FLRW model with the parameters, Hubble constant and matter density. Note this is a two parameter fit and presumes A CURVED UNIVERSE GEOMETRY WITHOUT DARK ENERGY should give very similar results to Vers5 but without integration.
-###########
+"""
+def model(x, Hubble, Matter):
+    return 5*(np.log10((_portion1(x, Hubble, Matter))*(_portion3(x, Matter))))+25
+
+"""
 This _portion2 is the subfunction to be subjected to the sinh function
-"""  
+"""
 def _portion2(x, Matter):
     return 2*(np.arctanh(np.sqrt(np.fabs(1-Matter)))- np.arctanh(np.sqrt(np.fabs(1-Matter))/np.sqrt(1+Matter*x)))
 """
@@ -28,11 +28,3 @@ This _portion1 of the function is a simple calculation
 """
 def _portion1(x, Hubble, Matter):
     return ((299792*(1+x))/(Hubble*np.sqrt(np.fabs(1-Matter))))
-"""
-Putting the complete function together as the cmpltresult. Note this routine only uses the numpy module.
-"""
-def model(m_B, Hubble, Matter):
-    return 5*(np.log10((_portion1(m_B, Hubble, Matter))*(_portion3(m_B, Matter))))+25
-"""
-TypeError: unsupported operand type(s) for *: 'function' and 'function' BUT AGAIN I DON'T KNOW WHY?' Supposedly something wrong with cmpltresult calculation?
-"""
